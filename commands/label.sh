847acc0cc8cf5d173358987f9ef4985c87df3ada
Márton Elek <elek@apache.org>
2020-02-10 14:42:10 +0100

    silent curl

--- commands/label.sh
+++ commands/label.sh
@@ -2,7 +2,7 @@
 #doc: add new label to the issue: `/label <label>`
 LABEL="$1"
 URL="$(jq -r '.issue.url' $GITHUB_EVENT_PATH)/labels"
-curl \
+curl -s \
        -X POST \
        --data "$(jq --arg value "$LABEL" -n '{labels: [ $value ]}')" \
        --header "authorization: Bearer $GITHUB_TOKEN" \
--- commands/ready.sh
+++ commands/ready.sh
@@ -6,7 +6,7 @@ curl -s https://api.github.com/repos/elek/comment-controll/pulls/1/reviews \
     | jq -r '.[] | [.user.login, .id] | @tsv' \
     | grep github-actions \
     | awk '{print $2}' \
-    | xargs -n1 -IISSUE_ID curl \
+    | xargs -n1 -IISSUE_ID curl -s \
        -X PUT \
        --data "$(jq --arg message "$MESSAGE" -n '{message: $message}')" \
        --header "authorization: Bearer $GITHUB_TOKEN" \

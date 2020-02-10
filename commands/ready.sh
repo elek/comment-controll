779ce0031ad5cad98258a093c11fcec5562c79b0
Márton Elek <elek@apache.org>
2020-02-10 14:52:47 +0100

    fix ready command

--- commands/ready.sh
+++ commands/ready.sh
@@ -2,11 +2,11 @@
 #doc: Dismiss all the blocking reviews by github-actions bot
 MESSAGE="Blocking review request is removed."
 URL="$(jq -r '.issue.pull_request.url' $GITHUB_EVENT_PATH)/reviews"
-curl -s -o /dev/null https://api.github.com/repos/elek/comment-controll/pulls/1/reviews \
+curl -s -o /dev/null "$URL" \
     | jq -r '.[] | [.user.login, .id] | @tsv' \
     | grep github-actions \
     | awk '{print $2}' \
-    | xargs -n1 -IISSUE_ID curl -s \
+    | xargs -n1 -IISSUE_ID curl -s -o /dev/null \
        -X PUT \
        --data "$(jq --arg message "$MESSAGE" -n '{message: $message}')" \
        --header "authorization: Bearer $GITHUB_TOKEN" \

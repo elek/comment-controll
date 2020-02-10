76d16b4879be1d783f20fe3769010316cceca0d0
Márton Elek <elek@apache.org>
2020-02-10 13:11:26 +0100

    fix error handling

--- process-comment.sh
+++ process-comment.sh
@@ -6,10 +6,13 @@ if [ "$LINES" == "0" ]; then
       echo "Command $BODY is received"
       COMMAND=$(echo $BODY | awk '{print $1}' | sed 's/\///')
       if [ -f "commands/$COMMAND.sh" ]; then
-         RESPONSE=$("./commands/$COMMAND.sh")
-         COMMENTS_URL=$(jq -r .issue.comments_url $GITHUB_EVENT_PATH)
-         set +x #do not display the GITHUB_TOKEN
-         curl -s \
+         RESPONSE=$("./commands/$COMMAND.sh 2>&1")
+      else
+         RESPONSE="No such commands \n\n$(./commands/help.sh)"
+      fi
+      set +x #do not display the GITHUB_TOKEN
+      COMMENTS_URL=$(jq -r .issue.comments_url $GITHUB_EVENT_PATH)
+      curl -s \
             --data "$(jq --arg body "$RESPONSE" -n '{body: $body}')" \
             --header "authorization: Bearer $GITHUB_TOKEN" \
             --header 'content-type: application/json' \
